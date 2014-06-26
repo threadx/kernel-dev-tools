adec93e03b188ce5604d877d0b8e8ab598fea75f
Howard Young <hyoung@clkngo.com>
2014-06-26 17:44:40 +0000

    Added install script for Ubuntu

---
+++ os_ubuntu_install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# Kernel development tools install for Ubuntu 14
+#
+
+#
+# Update to latest patches
+#
+sudo apt-get update
+sudo apt-get upgrade
+
+#
+#
+sudo apt-get install -y linux-headers-$(uname -r) build-essential
+
+#
+# NFS
+#
+
+sudo apt-get install -y nfs-kernel-server
+sudo apt-get install -y nfs-common
+
+#
+# Ohter missing utilities
+#
+
+sudo apt-get install -y git
+sudo apt-get install -y unzip
+sudo apt-get install -y nginx
+sudo apt-get install -y php5-fpm
+sudo apt-get install -y htop
+sudo apt-get install -y pandoc
+sudo apt-get install -y make
+sudo apt-get install -y g++
+sudo apt-get install -y libzip-dev
+sudo apt-get install -y libcurl4-openssl-dev
+sudo apt-get install -y libxml2-dev
+sudo apt-get install -y xutils-dev
+
+#
+# Install vim to replate vi
+#
+
+sudo apt-get install -y vim
