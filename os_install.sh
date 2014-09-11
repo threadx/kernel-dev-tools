fa7bbe5fc72e89e846625d7e03a23cf6e89f3eba
threadx <hyoung@clkngo.com>
2014-09-11 10:30:25 -0700

    updated for ZeroMQ and czmq wrapper lib

--- os_install.sh
+++ os_install.sh
@@ -68,6 +68,18 @@ tar xvvf zeromq-4.0.4.tar
 cd zeromq-4.0.4
 ./configure
 make
-make install
-ldconfig
+sudo make install
+sudo ldconfig
 cd ..
+
+git clone git://github.com/zeromq/czmq.git
+cd czmq
+sh autogen.sh
+./configure
+make all
+sudo make install
+sudo ldconfig
+cd src
+./czmq_selftest
+cd ../..
+
--- os_ubuntu_install.sh
+++ os_ubuntu_install.sh
@@ -36,9 +36,37 @@ sudo apt-get install -y libzip-dev
 sudo apt-get install -y libcurl4-openssl-dev
 sudo apt-get install -y libxml2-dev
 sudo apt-get install -y xutils-dev
+sudo apt-get install -y libtool
+sudo apt-get install -y automake
 
 #
 # Install vim to replate vi
 #
 
 sudo apt-get install -y vim
+
+#
+# ZeroMQ
+#
+
+wget http://download.zeromq.org/zeromq-4.0.4.tar.gz
+gunzip zeromq-4.0.4.tar.gz
+tar xvvf zeromq-4.0.4.tar
+cd zeromq-4.0.4
+./configure
+make
+sudo make install
+sudo ldconfig
+cd ..
+
+git clone git://github.com/zeromq/czmq.git
+cd czmq
+sh autogen.sh
+./configure
+make all
+sudo make install
+sudo ldconfig
+cd src
+./czmq_selftest
+cd ../..
+
